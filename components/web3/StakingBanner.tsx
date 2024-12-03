8733f3348af9cad9d51d6a51946807e180063d65
Giulianomlodi <giulianomlodi@gmail.com>
2024-12-03 10:35:28 +0100

    changes to homepage

--- components/web3/StakingBanner.tsx
+++ components/web3/StakingBanner.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import Image, { ImageProps } from 'next/image';
+import Link from 'next/link';
 
 // Add props to the component, including ImageProps for type safety
 const StakingBanner: React.FC<ImageProps> = (props) => (
@@ -25,14 +26,18 @@ const StakingBanner: React.FC<ImageProps> = (props) => (
             <h1 className="text-4xl font-bold text-white tracking-wider text-center mt-20 mb-4">
                 Welcome to Other Italy
             </h1>
-            <p className="text-lg text-white text-center mt-4 mb-40">
+            <p className="text-lg text-white text-center mt-4 mb-10 ">
                 <strong>OTHER ITALY</strong> is the Italian community of all Yuga asset. We focus on various aspects one of the main ones is gaming. In fact, we are structured by specialty in every branch and sector where Yuga assets are involved. In addition, we tell stories through our comics by joining with other projects and MBAs in addition to organizing IRL events.
                 <br />  <br />
                 The Other Italy membership token It’s a free mint, and allow you to be part of an even smaller and closer-knit community, it is not enough to be a Yuga asset holder to have it you must be chosen and selected as a commendable member of the same team in order to have the opportunity to be part of the Club.
                 <br />  <br />
                 Needless to say, our token will give you access to some exclusive “things” 🤣
             </p>
+            <Link href="/mint" className=" mb-40 px-6 py-2 bg-[#000C54] text-white rounded hover:opacity-70 transition">
+                Mint your membership Now
+            </Link>
         </div>
+
     </>
 );
 
